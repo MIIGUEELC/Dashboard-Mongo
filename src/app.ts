69569cc56b5dc564f7f187fe013ab2808d5e6908
MIIGUEELC <miigueel9219@gmail.com>
2025-01-28 10:53:58 +0100

    arreglos dashboard

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import express, { Response, Request } from "express";
+import express, { Response, Request,NextFunction} from "express";
 import dotenv from 'dotenv';
+import cors from 'cors'; 
 import authRoutes from './routes/authRoutes'
 import protectedRoutes from './routes/protectedRoutes'
 import publicRouter from "./routes/publicRoutes";
 import connectDB from "./database";
 
 const serverless = require('serverless-http')
-
+const PORT = process.env.PORT || 3001;
 dotenv.config();
 
 connectDB();
@@ -14,6 +15,22 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+// Habilitar CORS
+
+app.use(cors({
+    origin: 'http://localhost:5500',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+// Solucionar problemas con preflight requests (CORS y Passport)
+app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200); // 
+    } else {
+        next();
+    }
+});
 
 //Rutas Publicas.
 app.use('/api', publicRouter)
@@ -22,7 +39,7 @@ app.use('/api', publicRouter)
 app.use('/api/auth', authRoutes);
 
 //Rutas Privadas, tener necesariamente un token valido.
-app.use('/api/protected', protectedRoutes);
+app.use('/api', protectedRoutes);
 
 //Ruta raíz para dar la Bienvenida.
 app.use('/', (req: Request, res: Response) => {
@@ -32,3 +49,7 @@ app.use('/', (req: Request, res: Response) => {
 export const handler = serverless(app);
 
 
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
